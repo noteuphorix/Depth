6ba9ad744398278c2d12f0bc38a8e39b5c02eb9e
noteuphorix <bswarek@gmail.com>
2026-02-04 14:00:29 -0500

    Hotfix

--- Depth.ps1
+++ Depth.ps1
@@ -433,7 +433,16 @@ function Install-O365 {
 # --- Function from Install-PassedWingetApp.ps1 ---
 function Install-PassedWingetApp {
     param([string]$AppID)
-    Start-Process winget -ArgumentList "install --id $AppID --silent --accept-source-agreements --accept-package-agreements" -Wait -PassThru -NoNewWindow
+
+    if ($AppID -eq "Dell.CommandUpdate" -or $AppID -eq "Dell.CommandUpdate.Universal") {
+        # Trigger full upgrade instead of specific install
+        Write-Host "Dell Command Update detected. Running full system upgrade..." -ForegroundColor Cyan
+        Start-Process winget -ArgumentList "upgrade --all --silent --accept-source-agreements --accept-package-agreements" -Wait -PassThru -NoNewWindow
+    }
+    else {
+        # Run the standard install for everything else
+        Start-Process winget -ArgumentList "install --id $AppID --silent --accept-source-agreements --accept-package-agreements" -Wait -PassThru -NoNewWindow
+    }
 }
 
 # --- Function from Refresh-Clients.ps1 ---
--- src/Functions/Install-PassedWingetApp.ps1
+++ src/Functions/Install-PassedWingetApp.ps1
@@ -1,4 +1,13 @@
 function Install-PassedWingetApp {
     param([string]$AppID)
-    Start-Process winget -ArgumentList "install --id $AppID --silent --accept-source-agreements --accept-package-agreements" -Wait -PassThru -NoNewWindow
+
+    if ($AppID -eq "Dell.CommandUpdate" -or $AppID -eq "Dell.CommandUpdate.Universal") {
+        # Trigger full upgrade instead of specific install
+        Write-Host "Dell Command Update detected. Running full system upgrade..." -ForegroundColor Cyan
+        Start-Process winget -ArgumentList "upgrade --all --silent --accept-source-agreements --accept-package-agreements" -Wait -PassThru -NoNewWindow
+    }
+    else {
+        # Run the standard install for everything else
+        Start-Process winget -ArgumentList "install --id $AppID --silent --accept-source-agreements --accept-package-agreements" -Wait -PassThru -NoNewWindow
+    }
 }
