fa2a4815e40aaff603b653e61b2726ff651d74a6
noteuphorix <bswarek@gmail.com>
2026-03-04 09:34:55 -0500

    Updated language pack uninstaller to include standard en-us

--- Depth.ps1
+++ Depth.ps1
@@ -845,7 +845,7 @@ function Uninstall-OfficeLanguagePacks {
     # 1. Get all Office ClickToRun entries, excluding English
     $OfficePacks = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" -ErrorAction SilentlyContinue | Where-Object {
         $_.UninstallString -like "*OfficeClickToRun.exe*" -and 
-        $_.DisplayName -notlike "*en-us*" -and 
+        #$_.DisplayName -notlike "*en-us*" -and 
         $_.DisplayName -notlike "*English*" -and 
         $_.DisplayName -ne $null
     }
