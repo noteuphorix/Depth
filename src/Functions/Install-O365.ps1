2ad861dfe2ba2799b52215f1838ba829e9ffd176
noteuphorix <bswarek@gmail.com>
2026-04-08 10:59:44 -0400

    Testing O365 install change with switch

--- src/Functions/Install-O365.ps1
+++ src/Functions/Install-O365.ps1
@@ -1,10 +1,14 @@
 function Install-O365 {
     Show-FunctionBanner "O365 Apps Install"
-    # Pre-defined list of IDs
     $Apps = @("Microsoft.Office")
 
     foreach ($App in $Apps) {
-        # Process runs and displays output in its own console window area
-        Start-Process winget -ArgumentList "install --id $App --silent --accept-source-agreements" -Wait -PassThru -NoNewWindow
+        $result = Start-Process winget -ArgumentList "install --id $App --silent --accept-source-agreements --accept-package-agreements" -Wait -PassThru -NoNewWindow
+        
+        switch ($result.ExitCode) {
+            0            { Write-Host "Successfully installed $App" -ForegroundColor Green }
+            -1978335189  { Write-Host "$App is already up to date" -ForegroundColor Cyan }
+            default      { Write-Warning "Failed to install $App (Exit code: $($result.ExitCode))" }
+        }
     }
 }
