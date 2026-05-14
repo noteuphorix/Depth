892271fbf4b3b26741ed6bbdba610dfd4fec43cf
noteuphorix <bswarek@gmail.com>
2026-05-14 15:41:29 -0400

    Hotfix to status light

--- Depth.ps1
+++ Depth.ps1
@@ -2498,16 +2498,14 @@ function Update-Status {
         [string]$State
     )
 
-    # Change the color of the StatusLight Ellipse
+    $ellipse = $sync.Main.FindName("Ellipse_StatusLight")
+
     if ($State -eq "Busy") {
-        # Use Red for Busy
-        $Ellipse_StatusLight.Fill = [System.Windows.Media.Brushes]::Red
+        $ellipse.Fill = [System.Windows.Media.Brushes]::Red
     } else {
-        # Use LimeGreen for Ready
-        $Ellipse_StatusLight.Fill = [System.Windows.Media.Brushes]::LimeGreen
+        $ellipse.Fill = [System.Windows.Media.Brushes]::LimeGreen
     }
 
-    # Keeps the UI responsive during the color change
     [System.Windows.Forms.Application]::DoEvents()
 }
 
